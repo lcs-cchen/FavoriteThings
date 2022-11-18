e4fcdff6035acd57f05ef0a3694c681c412e24e7
cyrus chen <pschen@lcs.on.ca>
2022-11-17 20:37:27 -0500

    added image box to list thing

--- FavoriteThings/View/FavouriteThingsListView.swift
+++ FavoriteThings/View/FavouriteThingsListView.swift
@@ -13,27 +13,27 @@ struct FavouriteThingsListView: View {
             NavigationLink(destination: {
                 BananaView()
             }, label: {
-                PhotoListView(listPhoto: "Banana", listTitle: "Banana", listSubtitle: "Subtitle")
+                PhotoListView(listPhoto: "Banana", listTitle: "Banana", listSubtitle: "My personal favorite fruit")
                     })
             NavigationLink(destination: {
                 StarWarsView()
             }, label: {
-               PhotoListView(listPhoto: "starWars", listTitle: "starwars", listSubtitle: "subtitle")
+               PhotoListView(listPhoto: "starWars", listTitle: "starwars", listSubtitle: "One of the best trilogys ever made if not the best")
             })
             NavigationLink(destination: {
                 XXXView()
             }, label: {
-                PhotoListView(listPhoto: "xxx", listTitle: "'?' Album", listSubtitle: "In my opinion the best album made by one of the best artist of our time")
+                PhotoListView(listPhoto: "xxx", listTitle: "'?' Album", listSubtitle: "One the best album made by one of the best artist of our time")
             })
             NavigationLink(destination: {
                 TorontoMapleLeafView()
             }, label: {
-                PhotoListView(listPhoto: "Toronto", listTitle: "Toronto Maple Leafs", listSubtitle: "subtitle")
+                PhotoListView(listPhoto: "Toronto", listTitle: "Toronto Maple Leafs", listSubtitle: "The team I support onced I moved to lakefield")
             })
             NavigationLink(destination: {
                 CatView()
             }, label: {
-                PhotoListView(listPhoto: "cat", listTitle: "Cats", listSubtitle: "subtitle")
+                PhotoListView(listPhoto: "cat", listTitle: "Cats", listSubtitle: "Cats are the best pets no questions asked")
             })
             
             
